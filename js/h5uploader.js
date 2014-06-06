4a470830f97e48f26a18661b68da7d39d2f91c09
dom garcia <dom.garcia@nuworks.ph>
2014-06-06 19:57:43 +0800

    fixes

--- js/h5uploader.js
+++ js/h5uploader.js
@@ -2,7 +2,8 @@ jQuery.fn.h5u = function(obj)
 {
 	var version = '1.1.5';
 	this.el = $(this[0]);
-		var option = {
+		var option = { 
+			wurl		: "",
 			hasRotation : true,
 			hasZoom     : true,
 			hasPinch	: true,
@@ -21,49 +22,141 @@ jQuery.fn.h5u = function(obj)
 
 		this.HTMLview = function()
 		{
-			// var container = el;
-				this.el.addClass('canvas-size');
-			var canvasWidth = this.el.width();
+			this.el.addClass('h5u-canvas-size');
+			var canvasWidth  = this.el.width();
 			var canvasHeight = this.el.height();
 
-			
-			this.el.append("<div class='right-nav'></div>");
-			var rightNav = self.el.find('.right-nav');
+			this.el.append("<div id='h5u-error-wrapper'>"
+						  +"<div class='h5u-error-content'>0%</div>"
+						  +"</div>");
+
+			this.el.find("#h5u-error-wrapper").css({ width: canvasWidth, height: canvasHeight });
+			this.el.find("#h5u-error-wrapper").hide();
+
+			this.el.append("<div class='h5u-right-nav'></div>");
+			var rightNav = self.el.find('.h5u-right-nav');
 
 			if(option.hasZoom){
-				rightNav.append("<button id='zoomin' class='right-nav-des'>"+option.zoomInCaption+"</button>"
-							   +"<button id='zoomout' class='right-nav-des'>"+option.zoomOutCaption+"</button>");
+				rightNav.append("<button id='h5u-zoomin' class='h5u-right-nav-des'>"+option.zoomInCaption+"</button>"
+							   +"<button id='h5u-zoomout' class='h5u-right-nav-des'>"+option.zoomOutCaption+"</button>");
 			}
 
 			if(option.hasRotation){
-				rightNav.append("<button id='rotleft' class='right-nav-des'>"+option.rotateIncreaseCaption+"</button>"
-							   +"<button id='rotright' class='right-nav-des'>"+option.rotateDecreaseCaption+"</button>");
+				rightNav.append("<button id='h5u-rotleft' class='h5u-right-nav-des'>"+option.rotateIncreaseCaption+"</button>"
+							   +"<button id='h5u-rotright' class='h5u-right-nav-des'>"+option.rotateDecreaseCaption+"</button>");
 			}
 
-			this.el.append("<canvas id='canvas-base' width='"+canvasWidth
+			this.el.append("<canvas id='h5u-canvas-base' width='"+canvasWidth
 							+"' height='"+canvasHeight+"'></canvas>");
 
-			this.el.append("<input type='file' id='file-input' name='files[]' multiple/>")
-					 .append("<br style='clear:both'/>")
-					 .append("<div style='width: auto;'>"
-					 		+"<button id='browse-btn' class='bottom-nav'>Browse File</button>"
-					 		+"<button id='submit-btn' class='bottom-nav'>Submit Entry</button>"
-					 		+"</div>");
+			this.el.append("<input type='file' id='h5u-file-input' name='files[]' multiple/>"
+					 		+"<br style='clear:both'/>"
+					 		+"<div style='width: auto;'>"
 
+					 		//preloader
+					 		+"<div class='h5u-tpreload-wrapper'>"
+						    +"<div class='h5u-tpreload-bar'></div>"
+						    +"</div>"
+							
+							//button nav
+							+"<button id='h5u-browse-btn' class='h5u-bottom-nav'>Browse File</button>"
+					 		+"<button id='h5u-submit-btn' class='h5u-bottom-nav'>Submit Entry</button>"
+					 		+"</div>");
+			//version
 			this.el.append("<br style='clear:both;'/><p>version "+version+"</p>");
+
+			this.el.find(".h5u-tpreload-wrapper").hide();
 		}
 
 		this.initCanvas = function()
 		{
 			var that = this;
-			this.el.find('#browse-btn').on("mouseup",function( evt ){
-				self.el.find('#file-input').click();
+
+			this.updatePreloader = function( curVal ){
+				var newProp = { width : curVal+"%" }
+					self.el.find('.h5u-tpreload-bar').css( newProp );
+					curVal = Math.floor(curVal);
+					self.el.find('.h5u-error-content').html( curVal + "%" );
+			}
+
+			this.processAjax = function(imageB64) {
+				var request = $.ajax({
+					headers: {
+				        Accept : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+				    },
+					xhr: function() {
+						var _xhr;
+						if (window.XMLHttpRequest) {
+						// code for IE7+, Firefox, Chrome, Opera, Safari
+							_xhr=new XMLHttpRequest();
+						} else { // code for IE6, IE5
+							_xhr=new ActiveXObject("Microsoft.XMLHTTP");
+						}
+					    _xhr.upload.addEventListener("progress", function(evt) {
+					        if (evt.lengthComputable) {
+					            var percentComplete = ( evt.loaded / evt.total ) * 100 ;
+					            //preloader update
+					          	that.updatePreloader( percentComplete );
+					        }
+					   	}, false );
+						return _xhr;
+					},
+
+					type: "POST",
+				  	url: option.wurl.toString() , // "http://nwshare.ph/goldilocks/goldigoodtimes/easel/servertest.php",
+					processData: true,
+					cache: false,
+					contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+				  	data: imageB64,
+
+					success: function(){
+						self.find("#h5u-error-wrapper").hide();
+						console.log('loaded');
+					}
+				});
+				//request.done( function ( msg ) { console.log("request-successful", msg ) });
+				request.fail( function( jqXHR, textStatus ) { console.log("request-error", textStatus ) } );
+			}
+
+			this.dataURItoBlob = function(dataURI) {
+				// convert base64 to raw binary data held in a string
+				// doesn't handle URLEncoded DataURIs
+				var byteString;
+				if (dataURI.split(',')[0].indexOf('base64') >= 0)
+				    byteString = atob(dataURI.split(',')[1]);
+				else
+				    byteString = unescape(dataURI.split(',')[1]);
+				// separate out the mime component
+				var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+				// write the bytes of the string to an ArrayBuffer
+				var ab = new ArrayBuffer(byteString.length);
+				var ia = new Uint8Array(ab);
+				for (var i = 0; i < byteString.length; i++) {
+				    ia[i] = byteString.charCodeAt(i);
+				}
+
+				// write the ArrayBuffer to a blob, and you're done
+				return new Blob([ab],{type: mimeString});
+			}
+
+			this.el.find('#h5u-browse-btn').on("mouseup",function( evt ){
+				self.el.find('#h5u-file-input').click();
 			});
 
-			this.el.find('#submit-btn').on("mouseup",function( evt ){
+			this.el.find('#h5u-submit-btn').on("mouseup",function( evt ){
 				if(!hasImg) return;
 				var dataURL = canvas.toDataURL();
-				self._callback(dataURL);
+				var binary = that.dataURItoBlob( dataURL );
+				
+				self.el.find(".h5u-tpreload-wrapper").stop().slideDown(
+					500, 
+					function(){ 
+						//self._callback( binary );
+						that.updatePreloader(1);
+						self.processAjax( dataURL );
+						self.el.find("#h5u-error-wrapper").show();
+					}
+				);
 			});
 
 			//Canvas 
@@ -75,7 +168,7 @@ jQuery.fn.h5u = function(obj)
 			var imgConvert = new Image();
 			var originalRegistry = [];
 
-			canvas = document.getElementById('canvas-base');
+			canvas = document.getElementById('h5u-canvas-base');
 			stage = new createjs.Stage(canvas);
 			
 			createjs.Touch.enable(stage);
@@ -106,6 +199,8 @@ jQuery.fn.h5u = function(obj)
 			this.copyToCanvas = function ( value ) {
 				if(bitmapHolder) that.resetImg();
 
+				that.updatePreloader(1);
+				
 				hasImg = true;
 				imgConvert = new Image();
 				imgConvert.onload = that.bitmapReady;
@@ -136,7 +231,7 @@ jQuery.fn.h5u = function(obj)
 			
 			this.controls = function(){
 				//scale
-				self.el.find('#zoomout').on("mouseup",function( evt ){
+				self.el.find('#h5u-zoomout').on("mouseup",function( evt ){
 					if(!hasImg) return;
 						if(cont.scaleX<0.3)return;
 						cont.scaleX -= option.scaleValue;
@@ -144,7 +239,7 @@ jQuery.fn.h5u = function(obj)
 						stage.update( evt );
 
 				});
-				self.el.find('#zoomin').on("mouseup",function( evt ){
+				self.el.find('#h5u-zoomin').on("mouseup",function( evt ){
 					if(!hasImg) return;
 						
 						cont.scaleX += option.scaleValue;
@@ -154,13 +249,13 @@ jQuery.fn.h5u = function(obj)
 				});
 
 				//rotation
-				self.el.find('#rotright').on("mouseup",function(){
+				self.el.find('#h5u-rotright').on("mouseup",function(){
 					if(!hasImg) return;
 						cont.rotation+= option.rotateValue;
 						stage.update();
 				});
 
-				self.el.find('#rotleft').on("mouseup",function(){
+				self.el.find('#h5u-rotleft').on("mouseup",function(){
 					if(!hasImg) return;
 						cont.rotation-= option.rotateValue;
 						stage.update();
@@ -232,7 +327,7 @@ jQuery.fn.h5u = function(obj)
 	  			});
 			}
 
-			self.el.find('#file-input').change( function(evt){
+			self.el.find('#h5u-file-input').change( function(evt){
 				var fl = evt.target.files;
 				var o = [];
 				var r = new FileReader();
@@ -249,6 +344,8 @@ jQuery.fn.h5u = function(obj)
 				}
 			} );
 			that.controls();
+
+
 		}
 
 		this.addCallback = function(callback) {
